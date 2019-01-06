2584b845d012cd7be1cd1077b8e51e89ea6af78d
dwighson <dwighson@gmail.com>
2019-01-06 20:29:56 +0100

    feedback changes

--- components/Contact.vue
+++ components/Contact.vue
@@ -2,14 +2,14 @@
   <div class="row contactwrap">
     <h1>Contact us</h1>
     <hr>
-    <p>Contact us for appointment or more information</p>
+    <p>Contact us for an appointment or more information</p>
     <div class="col-9 center">
       <div class="contact">
-        <input type="text" placeholder="Name">
-        <input type="text" placeholder="Company Name">
-        <input type="text" placeholder="E-mail">
-        <textarea name id placeholder="message"></textarea>
-        <button>
+        <input v-model="name" type="text" placeholder="Name">
+        <input v-model="companyName" type="text" placeholder="Company Name">
+        <input v-model="email" type="email" placeholder="E-mail">
+        <textarea v-model="msg" name id placeholder="message"></textarea>
+        <button v-on:click="sendForm">
           <Sendicon></Sendicon>
         </button>
       </div>
@@ -17,34 +17,33 @@
         <ul>
           <li>
             <div class="icon">
-              
-              <img src="~assets/locationicon.svg" alt> 
+              <img src="~assets/locationicon.svg" alt>
             </div>
-            <p>text</p>
+            <p>Anchor House New Road, Suite 3, DY2 9AF ENGLAND</p>
           </li>
           <li>
             <div class="icon">
               <img src="~assets/phoneicon.svg" alt>
             </div>
-            <p>text</p>
+            <p>currently not available.</p>
           </li>
-          <li>
+          <!-- <li>
             <div class="icon">
               <img src="~assets/emailicon.svg" alt>
             </div>
             <p>text</p>
-          </li>
+          </li>-->
           <li>
             <div class="icon">
               <img src="~assets/facebookicon.svg" alt>
             </div>
-            <p>text</p>
+            <p>eloraserviceslimited</p>
           </li>
           <li>
             <div class="icon">
               <img src="~assets/instagramicon.svg" alt>
             </div>
-            <p>text</p>
+            <p>eloraserviceslimited</p>
           </li>
         </ul>
       </div>
@@ -53,10 +52,48 @@
 </template>
 <script>
 import Sendicon from "@/components/Sendicon";
-import emailjs from 'emailjs-com'
+import emailjs from "emailjs-com";
 export default {
+  data() {
+    return {
+      name: "",
+      companyName: "",
+      email: "",
+      msg: ""
+    };
+  },
   components: {
     Sendicon
+  },
+  methods: {
+    sendForm: function() {
+      var template_params = {
+        reply_to: this.email,
+        from_name: this.name + "(" + this.companyName + ")",
+        to_name: "Patience",
+        message_html: this.msg
+      };
+
+      var service_id = "default_service";
+      var template_id = "template_ypR1fahP";
+      if (
+        !this.name == "" &&
+        !this.companyName == "" &&
+        !this.email == "" &&
+        !this.msg == ""
+      ) {
+        if (
+          this.email.indexOf("@") == true &&
+          this.email.indexOf("@") < this.email.indexOf(".")
+        ) {
+          emailjs.send(service_id, template_id, template_params);
+        } else {
+          alert("make sure to fill in a correct email address.");
+        }
+      } else {
+        alert("Make sure to fill out everything!");
+      }
+    }
   }
 };
 </script>
@@ -65,6 +102,15 @@ export default {
 .contactwrap {
   text-align: center;
   padding-top: 200px;
+}
+h1 {
+  font-size: 50px;
+}
+hr {
+  height: 8px;
+  width:  200px;
+  border: none;
+  background: blue;
 }
 .contactwrap hr {
   margin: 0 auto;
@@ -87,7 +133,6 @@ export default {
   text-align: left;
   flex: 1;
   height: 500px;
-  background: green;
 }
 .contact {
   height: 500px;
@@ -95,6 +140,9 @@ export default {
   position: relative;
   box-sizing: border-box;
   padding: 10px;
+}
+.center {
+  padding-top: 50px;
 }
 button {
   border-radius: 50%;
@@ -108,6 +156,8 @@ button {
 }
 li {
   display: flex;
+  /* background: purple; */
+  margin-bottom: 20px;
 }
 input {
   height: 40px;
--- components/Container.vue
+++ components/Container.vue
@@ -14,9 +14,6 @@
           </div>
           <button v-on:click="togglereadmore" v-if="readmoretoggle == false">Read more</button>
         </div>
-        <div class="col-3 containerimg">
-          <Cleaningimg></Cleaningimg>
-        </div>
       </div>
     </div>
     <div class="wrapper">
@@ -27,6 +24,51 @@
       </div>
     </div>
     <div class="container">
+      <div class="row about">
+        <div class="col-8 aboutwrap">
+          <h1>ABOUT US</h1>
+          <hr>
+          <p>
+            We believe a clean working and living environment is a healthy working and living environment.
+            Our systematic approach to establishing and maintaining the highest standards of cleanliness
+            means you can focus on your work or career. Quality control is very important to us. Whatever
+            your cleaning and maintenances needs, whether weekly, monthly, and yearly cleaning /
+            maintenance that guarantees results – we are the right service for you. We offer short term or
+            paid trial period
+          </p>
+          <button v-on:click="togglereadmore2" v-if="readmoretoggle2 == false">Read more</button>
+          
+          <span class="readmore" v-show="readmoretoggle2">
+            <p>
+              Elora Cleaning and Maintenance services limited offers a full range of cleaning services to suit
+              any business and any budget. From dusting and cleaning home, small and medium-sized offices
+              to post- construction cleaning, our highly trained staff will ensure your environment is pleasant,
+              safe, and healthy. A welcoming and sanitised working environment boosts morale, productivity
+              and helps to ensure the health and wellbeing of your employees.
+              <br>Domestic and commercial cleaning can be a disruptive process, unless you hire the right team.
+              Having a clean and hygienic office space is an essential aspect of running a successful business,
+              of any size that’s where Elora domestic and commercial cleaning Services comes in. We use
+              eco-products because we believe it is important to have a clean living or working spaces with
+              minimal chemical residue. As well as maintaining an exceptional standard of hygiene and
+              cleanliness, our team have a keen eye for detail that will boost the aesthetic appeal of your office
+              space.
+              <br>We hire only the most conscientious and professional candidates. Our operatives are trained,
+              DBS vetted with full reference and insured.
+            </p>
+            <p>
+              ll reference and insured.
+              We offer short-term paid trial periods to ensure that you are happy with the level of our work.
+              Contact us today for a no obligation free quotation, no cleaning or maintenance job is too small.
+              We cover West midlands and surrounding areas so please feel free to get in touch and we will
+              be able to confirm availability a local operative with you.
+              Company registered in England &amp; Wales - No 11328289
+            </p>
+          </span>
+        </div>
+        <div class="col-3">
+          <div class="img"></div>
+        </div>
+      </div>
       <div class="row tenancy">
         <div class="col-8 tenancywrap">
           <h1>END OF TENANCY CLEANING</h1>
@@ -35,10 +77,11 @@
           <p>Our end of tenancy cleans are fully comprehensive and can often be completed within 48 hours of engagement, we offer a free key collection/delivery service within 5 miles of the property.</p>
           <p>We cover the entire midlands and surrounding areas so please feel free to get in touch and we will be able to confirm availability with you.</p>
         </div>
-        <div class="col-3 blocks">
-          <Popupblocks></Popupblocks>
+        <div class="col-3 containerimg">
+          <Cleaningimg></Cleaningimg>
         </div>
       </div>
+
       <Contact></Contact>
     </div>
     <footer></footer>
@@ -61,13 +104,16 @@ export default {
   },
   data() {
     return {
-      readmoretoggle: false
-    }
+      readmoretoggle: false,
+      readmoretoggle2: false
+    };
   },
   methods: {
     togglereadmore: function() {
-      alert("??")
-      this.readmoretoggle =! this.readmoretoggle
+      this.readmoretoggle = !this.readmoretoggle;
+    },
+    togglereadmore2: function() {
+      this.readmoretoggle2 = !this.readmoretoggle2;
     }
   }
 };
@@ -94,6 +140,21 @@ Cleaningimg {
 .containertxt {
   /* padding-bottom: 100px; */
 }
+.aboutwrap {
+  /* text-align: justify; */
+}
+.about {
+  margin-top: 100px;
+}
+.img {
+  height: 500px;
+  max-width: 400px;
+  width: 400px;
+  background: url("https://s3.us-west-2.amazonaws.com/secure.notion-static.com/b450fb4d-959e-4de5-a112-d27c1add3d61/rawpixel-777268-unsplash.jpg?AWSAccessKeyId=ASIAT73L2G45GU7VXGH2&Expires=1546882044&Signature=cdZvFmSc0a94FyQ60H5j4nc2FNQ%3D&x-amz-security-token=FQoGZXIvYXdzEPL%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaDIylNBXwPi4O6zXC9yK3A8h%2FGbWEQD5ZANHFAFKyaaRCBTj4lde78dksW8A7J6DVdFrUdFZdkvK6PjEFCl%2BANWaq6A8KNgPpaaUZRwwW%2BpfPrAJkZAjtNQEw2AMhkKdqzbb8u94M1SKiti8F2uq6DJ6hI1v4lobPJUO9JEa8zH7fC69LUJHN5VPes2IanAgY97nLhuLO237yO4egz%2FxgsslA6KtUzSaDOvIzb84qDQXXc6ZMpaIsbtT15KNvUuIrWaD%2FIpJw%2F5B7A%2FElhIzWKPW5wB2%2BtgtsOOrbWgZdWOSOnORqTixJhyIhzw%2F3xL6VJPb6ywHJpcBuM41uMqoSni33dAWuOoZeQ%2B%2BHgqDQJC%2FCzevs5au7NjIVUBIT3QURpWiMIUQjrcRV7hHUo%2FGfvV79nA97OkgZ3hoSCmfGpf6%2FsidnSGzy9PHWZDXtAJOdLRgmfDtwKft8%2B1Z1k%2FSaCQgmcgRf%2FbipuNG%2BtKJtX2qAY%2B1%2FPtRzsvBok%2B0TwLizmgLf%2FkCxC2bPCfSetBAdzNeAQHTR8acet1Cn4Jg4Y1V4%2FfjTtpVzgdzqUQwhSduiSS9jkWGqlsTvhNQPtRqCeW1zcR5FQvsons%2FI4QU%3D")
+    no-repeat center center;
+  background-size: cover;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);
+}
 .domesticwrap {
   padding-top: 100px;
   min-height: 700px;
@@ -103,7 +164,7 @@ Cleaningimg {
 }
 .readmore {
   min-height: 400px;
-  transition: all .5s ease;
+  transition: all 0.5s ease;
 }
 .testimonials {
   width: 100%;
@@ -163,6 +224,16 @@ button {
     margin-left: 50%;
     transform: translateX(-50%);
     text-align: left;
+  }
+  .img {
+    display: none;
+  }
+  .about {
+  }
+  .aboutwrap {
+    transform: translateX(-50%);
+    display: inline;
+    margin-left: 50%;
   }
 }
 </style>
